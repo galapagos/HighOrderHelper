12153c96c854d7bf189125ad7801888949f60191
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2017-05-10 14:43:22 +0900

    Initial Commit

---
+++ HighOrderHelper/HighOrderHelper.h
@@ -0,0 +1,19 @@
+//
+//  HighOrderHelper.h
+//  HighOrderHelper
+//
+//  Created by 本柳賢太 on 2017/05/10.
+//  Copyright © 2017年 k-motoyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HighOrderHelper.
+FOUNDATION_EXPORT double HighOrderHelperVersionNumber;
+
+//! Project version string for HighOrderHelper.
+FOUNDATION_EXPORT const unsigned char HighOrderHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HighOrderHelper/PublicHeader.h>
+
+
